48c8e5383b1372635ca6b25721ffbe78d2254522
lipeng <hlp1019878@163.com>
2019-10-16 14:46:23 +0800

    first

---
+++ src/layouts/Footer.js
@@ -0,0 +1,37 @@
+import React, { Fragment } from 'react';
+import { Layout, Icon } from 'antd';
+import GlobalFooter from '@/components/GlobalFooter';
+
+const { Footer } = Layout;
+const FooterView = () => (
+  <Footer style={{ padding: 0 }}>
+    <GlobalFooter
+      links={[
+        {
+          key: 'Pro 首页',
+          title: 'Pro 首页',
+          href: '#',
+          blankTarget: true,
+        },
+        {
+          key: 'github',
+          title: <Icon type="github" />,
+          href: '#',
+          blankTarget: true,
+        },
+        {
+          key: 'Ant Design',
+          title: 'Ant Design',
+          href: 'https://ant.design',
+          blankTarget: true,
+        },
+      ]}
+      copyright={
+        <Fragment>
+          Copyright <Icon type="copyright" /> HuLiPeng
+        </Fragment>
+      }
+    />
+  </Footer>
+);
+export default FooterView;
